f4af59da6c2c77add0c5e9fc4f2a5fb600c03037
Cody Harris <codyharris713@gmail.com>
2022-01-09 17:16:50 -0800

    Add files via upload

---
+++ Constructor/Start.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructor
+{
+    class Start
+    {
+        string name1 = "";
+        string id = "";
+        public Start(string name, string idx)
+        {
+            name1 = name;
+            id = idx;
+            Console.WriteLine("Hello {0}, {1}", name, idx);
+        }
+
+        public Start(string name) : this(name, "111") { }
+    }
+}
