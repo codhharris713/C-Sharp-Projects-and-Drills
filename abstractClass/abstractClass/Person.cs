f14b035d4cbcf91ced2a4dff0cad401c21f16a46
Cody Harris <codyharris713@gmail.com>
2022-01-09 17:12:14 -0800

    Add files via upload

---
+++ abstractClass/abstractClass/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace abstractClass
+{
+    public abstract class Person //abstract class
+    {
+        public string FirstName; //two properties of data type string
+        public string LastName;
+
+        public abstract void SayName(); //abstract method 
+    }
+}
