cdf225ea9a752d4f7fa2d5ff9b8448f358474206
Cody Harris <codyharris713@gmail.com>
2022-01-09 17:25:26 -0800

    Add files via upload

---
+++ overloadOpps/overloadOpps/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace overloadOpps
+{
+    class Employee
+    {
+        public int ID { get; set; }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            bool match = false;
+            if (employee1.ID == employee2.ID)
+            {
+                match = true;
+            }
+            return match;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            bool match = false;
+            if (employee1.ID != employee2.ID)
+            {
+                match = true;
+            }
+            return match;
+        }
+    }
+}
---
+++ overloadOpps/overloadOpps/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace overloadOpps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() { ID = 10 };
+            Employee employee2 = new Employee() { ID = 10 };
+
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("ID's match");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("ID's do not match");
+                Console.ReadLine();
+            }
+        }
+    }
+}
