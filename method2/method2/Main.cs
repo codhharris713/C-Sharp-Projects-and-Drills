376fe0ddd957253948921df472504d1f362af772
Cody Harris <codyharris713@gmail.com>
2022-01-09 17:26:16 -0800

    Add files via upload

---
+++ method2/method2/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace method2
+{
+    public class Main
+
+    {
+        public int Method1(int x, int y = 10)
+        {
+            int result = x * y;
+            return result;
+        }
+    }
+}
---
+++ method2/method2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace method2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Main i = new Main();
+
+            Console.WriteLine("Please enter number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You may choose another number or one will be provide for you.");
+            string num2 = Console.ReadLine();
+            if (num2 == "") //if user inputs nothing
+            {
+                Console.WriteLine(i.Method1(num1));//user input times 10
+            }
+            else
+            {
+                int num3 = Convert.ToInt32(num2);//user input number
+                Console.WriteLine(i.Method1(num1, num3));//both user numbers times
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
