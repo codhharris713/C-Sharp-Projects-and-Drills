4c75b610f6717c54278cb700a900ad6ee5fc37dc
Cody Harris <codyharris713@gmail.com>
2022-01-09 17:23:30 -0800

    Add files via upload

---
+++ incomeApp1/incomeApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace incomeApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal hourlyPay;
+            int weeklyHours;
+            decimal yearlySalary;
+            decimal yearlySalary1;
+
+            
+            
+            
+            Console.WriteLine("Anonymous Income Comparison Program");
+            
+            //prompt user to assign variables
+            Console.WriteLine("Person 1");
+            Console.Write("Hourly pay: $");
+            hourlyPay = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Hours worked each week: ");
+            weeklyHours = Convert.ToInt32(Console.ReadLine());
+
+            //calculate salaries and displays salaries
+            yearlySalary = hourlyPay * weeklyHours * 52;
+            Console.WriteLine("Annual salary of Person 1: {0:C}", yearlySalary);
+
+            Console.WriteLine("Person 2");
+            Console.Write("Hourly pay: $");
+            hourlyPay = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Hours worked each week: ");
+            weeklyHours = Convert.ToInt32(Console.ReadLine());
+
+            yearlySalary1 = hourlyPay * weeklyHours * 52;
+            Console.WriteLine("Annual salary of Person 2: {0:C}", yearlySalary1);
+
+            //Comparing person1 and person2 inputs
+            Console.WriteLine("Does Person 1 make more money then Person 2?");
+            bool compare = yearlySalary > yearlySalary1;
+            Console.WriteLine(compare);
+            Console.ReadLine();
+
+            
+
+
+        }
+    }
+}
