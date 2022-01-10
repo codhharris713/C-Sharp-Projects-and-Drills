50d542a3f28b708f7f890c330d62756dbb1671f4
Cody Harris <codyharris713@gmail.com>
2022-01-09 17:24:17 -0800

    Add files via upload

---
+++ inputLog/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace inputLog
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter a number:");
+            using (StreamWriter number = new StreamWriter(@"C:\Users\Cody\OneDrive\Documents\LogTest.txt", true))
+            {
+                number.WriteLine(Console.ReadLine());
+            }
+            string readBack = File.ReadAllText(@"C:\Users\Cody\OneDrive\Documents\LogTest.txt");
+            Console.WriteLine("\nYou have entered the number(s):\n" + readBack);
+            Console.ReadLine();
+
+        }
+    }
+}
